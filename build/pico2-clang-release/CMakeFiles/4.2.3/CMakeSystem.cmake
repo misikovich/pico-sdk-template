7eb057ed86d623ac125b600ee92cf13e55d8d60a
misikovich <misikovich.dev@gmail.com>
2026-07-17 11:52:28 +0200

    removed build folder

---
+++ build/pico2-clang-release/CMakeFiles/4.2.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-7.0.0-27-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "7.0.0-27-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/ss1/Projects/example-pico-project-rp2350/pico-sdk/cmake/preload/toolchains/pico_arm_cortex_m33_clang.cmake")
+
+set(CMAKE_SYSTEM "PICO")
+set(CMAKE_SYSTEM_NAME "PICO")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "cortex-m33")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
